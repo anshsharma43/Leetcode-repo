2bf6889043b481a144c2caa3003df0ea779c0043
Ansh <anshsharma.niku@gmail.com>
2022-05-23 14:51:07 +0530

    31

---
+++ 31. Next Permutation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& arr) 
+    {
+        int n=arr.size();
+        
+        int k,l;
+        
+        for(k=n-2;k>=0;k--)
+        {
+            if(arr[k] < arr[k+1])
+            {
+                break;
+            }
+        }
+        
+        if(k<0)
+        {
+            reverse(arr.begin(),arr.end());
+        }
+        
+        else
+        {
+            for(l=n-1;l>k;l--)
+            {
+                if(arr[l] > arr[k])
+                {
+                    break;
+                }
+            }
+            
+            swap(arr[k],arr[l]);
+            reverse(arr.begin()+k+1,arr.end());
+        }
+    }
+};
